d2a6531174301a96156ca6fd077e9040fcd12c58
Meghavati Chaudhari <meghapjc29@gmail.com>
2024-03-20 22:14:37 +0530

    task5:product sort and filter category implemented

--- react-app/src/features/product/ProductAPI.js
+++ react-app/src/features/product/ProductAPI.js
@@ -1,3 +1,5 @@
+import { isRejected } from "@reduxjs/toolkit";
+
 // A mock function to mimic making an async request for data
 export const fetchAllProducts = (amount = 1) => {
   return new Promise(async (resolve) =>{
@@ -8,3 +10,59 @@ export const fetchAllProducts = (amount = 1) => {
   }
   );
 }
+
+// export function fetchProductsByFilters(filter){
+//   return new Promise(async(resolve)=>{
+//     ///filter={"category":"smartphone"}
+//     // todo : on server will be supprt multiple ctaegory supprt
+//     let queryString='';
+//     for(let key in filter){
+//       queryString+=`${key}=${filter[key]}&`
+//     }
+//     // queryString = queryString.slice(0, -1);
+//     console.log(queryString);
+//     console.log(queryString)
+//     // we can use URLSearchParams(filter) method also.
+//     try{
+//       const response=await fetch('http://localhost:8080/products?'+queryString)
+//       const data= await response.json(); 
+//       resolve({data})
+//     }catch(error){
+//           isRejected(error)
+//     }
+    
+//   })
+// }
+
+export function fetchProductsByFilters(filter){
+  return new Promise(async(resolve,reject)=>{
+try{
+  
+  const queryString = new URLSearchParams(filter).toString();
+  console.log(queryString)
+
+  const response = await fetch('http://localhost:8080/products?' + queryString);
+      const data = await response.json();
+      resolve({ data });
+}
+    // we can use URLSearchParams(filter) method also.
+    catch (error) {
+      reject(error);
+    }
+    
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
--- react-app/src/features/product/ProductSlice.js
+++ react-app/src/features/product/ProductSlice.js
@@ -1,7 +1,7 @@
 // import { createAppSlice } from "../../app/createAppSlice"
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
-import { fetchAllProducts } from "./ProductAPI"
-
+import { fetchAllProducts , fetchProductsByFilters , fetchProductsByFilterssort} from "./ProductAPI"
+// import { configureStore } from '@reduxjs/toolkit';
 const initialState = {
   products:[],
   status: "idle",
@@ -15,36 +15,43 @@ export const fetchAllProductsAsync = createAsyncThunk(
   }
 )
 
+export const fetchProductsByFiltersAsync = createAsyncThunk(
+  'product/fetchProductsByFilters',
+  async(filter)=>{
+    const response = await fetchProductsByFilters(filter);
+    return response.data;
+  }
+)
+
 export const productSlice = createSlice({
   name: "product",
   initialState,
-  // reducers:
-  //  create => ({
-  //   increment: create.reducer(state => {
-  //     state.value += 1
-  //   }),
+  reducers:
+   create => ({
+    increment: create.reducer(state => {
+      state.value += 1
+    }),
     
     
-  //   incrementAsync: create.asyncThunk(
-  //     async amount => {
-  //       const response = await fetchCount(amount)
-  //       return response.data
-  //     },
-  //     {
-  //       pending: state => {
-  //         state.status = "loading"
-  //       },
-  //       fulfilled: (state, action) => {
-  //         state.status = "idle"
-  //         state.value += action.payload
-  //       },
-  //       rejected: state => {
-  //         state.status = "failed"
-  //       },
-  //     },
-  //   ),
-  // }),
-  
+    // incrementAsync: create.asyncThunk(
+    //   async amount => {
+    //     const response = await fetchCount(amount)
+    //     return response.data
+    //   },
+    //   {
+    //     pending: state => {
+    //       state.status = "loading"
+    //     },
+    //     fulfilled: (state, action) => {
+    //       state.status = "idle"
+    //       state.value += action.payload
+    //     },
+    //     rejected: state => {
+    //       state.status = "failed"
+    //     },
+    //   },
+    // ),
+  }),
   extraReducers:(builder)=>{
      builder
      .addCase(fetchAllProductsAsync.pending,(state)=>{
@@ -54,11 +61,18 @@ export const productSlice = createSlice({
       state.status='idle',
       state.products=action.payload;
      })
-  }
-  // selectors: {
-  //   selectCount: counter => counter.value,
-  //   selectStatus: counter => counter.status,
-  // },
+     .addCase(fetchProductsByFiltersAsync.pending,(state)=>{
+      state.status='loading';
+     })
+     .addCase(fetchProductsByFiltersAsync.fulfilled,(state,action)=>{
+      state.status='idle',
+      state.products=action.payload;
+     })
+  },
+  selectors: {
+    selectCount: counter => counter.value,
+    selectStatus: counter => counter.status,
+  },
 })
 
 
